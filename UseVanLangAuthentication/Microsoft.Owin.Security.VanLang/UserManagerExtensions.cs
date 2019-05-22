116850edbad56b70b46adf0b40e1875141a2bd11
Duy Khấu <duykhau1@vanlanguni.vn>
2019-05-23 00:43:04 +0700

    update to debug login

--- UseVanLangAuthentication/Microsoft.Owin.Security.VanLang/UserManagerExtensions.cs
+++ UseVanLangAuthentication/Microsoft.Owin.Security.VanLang/UserManagerExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
 
 namespace Microsoft.Owin.Security.VanLang
 {
@@ -18,12 +21,24 @@ namespace Microsoft.Owin.Security.VanLang
         {
             var user = Activator.CreateInstance(typeof(TUser)) as TUser;
             user.GetType().GetProperty("Email").SetValue(user, email);
+			var user1 = userManager.FindByEmail(email);
+			//xu lý 
             var result = await userManager.CreateAsync(user);
             if (result.Succeeded)
             {
                 var apiUrl = Constants.BaseUrl + "/API/getUserInfo?email=" + email;
-                // get id from api then set id to below
-                var infoLogin = new UserLoginInfo(Constants.DefaultAuthenticationName, "id???");
+				// get id from api then set id to below
+				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+				HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+				Stream receiveStream = response.GetResponseStream();
+				StreamReader readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+				string data = readStream.ReadToEnd().ToString();
+				response.Close();
+				readStream.Close();
+				UserProfile AspNetUser = JsonConvert.DeserializeObject<UserProfile>(data);
+
+				var infoLogin = new UserLoginInfo(Constants.DefaultAuthenticationName, AspNetUser.UserID);
                 result = await userManager.AddLoginAsync(user.Id, infoLogin);
                 if (result.Succeeded)
                     return user;
@@ -33,5 +48,16 @@ namespace Microsoft.Owin.Security.VanLang
             else
                 throw new IdentityException { IdentityResult = result };
         }
-    }
+		public class UserProfile
+		{
+			public string StID { get; set; }
+			public string FullName { get; set; }
+			public string PhoneNumber { get; set; }
+			public string Email { get; set; }
+			public Nullable<System.DateTime> DoB { get; set; }
+			public string UserID { get; set; }
+			public string Avatar { get; set; }
+
+		}
+	}
 }
---
+++ UseVanLangAuthentication/TestLoginProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestLoginProject.Startup))]
+namespace TestLoginProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
